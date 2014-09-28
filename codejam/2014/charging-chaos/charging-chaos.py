e2287f20b7f6d0211a33d9185df70eb4ef33c65b
Keith Peters <keith@monkeynova.com>
2014-09-27 17:16:02 -0700

    working O( N * L ) solution rather than brute O( 2 ** L )

--- codejam/2014/charging-chaos/charging-chaos.py
+++ codejam/2014/charging-chaos/charging-chaos.py
@@ -2,38 +2,59 @@
 
 import sys
 
-def bruteCaseSingle( bits, devices, outlets ):
-    def flipBits( outlet ):
-        def flipBit( bit ):
-            if bit in bits:
-                if outlet[bit] == "0":
-                    return "1"
-                else:
-                    return "0"
-            return outlet[bit]
-
-        return "".join( map( flipBit, xrange( len( outlet ) ) ) )
-
-    flipped = frozenset( map( flipBits, outlets ) )
-        
-    if devices == flipped:
-        return True
+def maybeFlipBit( maybe, c ):
+    if maybe:
+        if c == "1":
+            return "0"
+        else:
+            return "1"
     else:
-        return False
+        return c
 
-def bruteCase( L, devices, outlets ):
-    # print "Trying %d" % 2 ** L
-    best = None
+def flipBits( bits, outlet ):
+    return "".join( map( lambda x: maybeFlipBit( x in bits, outlet[x] ), xrange( len( outlet ) ) ) )
+
+def updateOutlets( outlets, removeOutlet, newBits ):
+    ret = []
+    for o in outlets:
+        if o != removeOutlet:
+            ret.append( flipBits( newBits, o ) )
+
+    return ret
+
+def neededBits( device, outlet, usedbits ):
+    newbits = frozenset( filter( lambda x: device[x] != outlet[x], xrange( len( device ) ) ) )
+    if len( newbits & usedbits ) > 0:
+        return None
+    else:
+        return newbits
+
+def findBestBits( devices, outlets ):
+    return nextDevice( devices, 0, outlets, frozenset() )
+
+def nextDevice( devices, deviceNum, outlets, bits ):
+    if deviceNum == len( devices ):
+        return bits
+
+    d = devices[ deviceNum ]
     bestBits = None
-    for i in xrange( 2 ** L ):
-        bits = frozenset( filter( lambda n: i & (1 << n) > 0, xrange( L ) ) )
-        if bestBits is None or len( bestBits ) > len( bits ):
-            # print "%d: %r" % ( i, bits )
-            if bruteCaseSingle( bits, devices, outlets ):
-                best = len( bits )
-                bestBits = bits
 
-    return best
+    for o in outlets:
+        newBits = neededBits( d, o, bits )
+        #print "%s (%s => %s): %r" % ( "  " * deviceNum, d, o, newBits )
+        if newBits is not None:
+            if len( newBits & bits ) > 0:
+                raise Exception( "newBits returned already used bits" )
+            newOutlets = updateOutlets( outlets, o, newBits )
+            finalBits = nextDevice( devices, deviceNum + 1, newOutlets, bits | newBits )
+            #print "%s %r" % ( "  " * deviceNum, finalBits )
+            if finalBits is not None:
+                if bestBits is None or len( finalBits ) < len( bestBits ):
+                    bestBits = finalBits
+
+    return bestBits
+
+
 
 def singleCase( caseNum ):
     config = sys.stdin.readline().split( " " )
@@ -59,10 +80,10 @@ def singleCase( caseNum ):
     if len( filter( lambda x: len( x ) != L, devices ) ) > 0:
         raise Exception( "second line of case %d contains entries if size other than %d" % ( caseNum, L ) )
 
-    best = bruteCase( L, frozenset( devices ), frozenset( outlets ) )
+    best = findBestBits( devices, outlets )
 
     if best is not None:
-        print "Case #%d: %d" % ( caseNum, best )
+        print "Case #%d: %d" % ( caseNum, len( best ) )
     else:
         print "Case #%d: NOT POSSIBLE" % caseNum
 
